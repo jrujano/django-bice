7e4f0b4bcbccda48ce1400e0511ea401c19ff1f8
Johan Rujano <johanrujano@gmail.com>
2020-04-14 06:05:14 -0400

    begin test

--- account/models.py
+++ account/models.py
@@ -10,3 +10,4 @@ from django.db import models
 class BiceUser(AbstractUser):
     twitter = models.CharField(blank=True, max_length=120)
     linkeidn = models.CharField(blank=True, max_length=25)
+    website = models.CharField(blank=True, max_length=250)
