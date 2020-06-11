4526745701d0b0c663f8418c92d9e0c2aba00951
Johan Rujano <johanrujano@gmail.com>
2020-06-11 00:58:22 -0400

    Administrador-mejoras

--- backend/settings.py
+++ backend/settings.py
@@ -176,3 +176,7 @@ TIME_ZONE = 'America/Santiago'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+LOGIN_URL = 'admin:login'
+LOGOUT_URL = '/logout'
+LOGIN_REDIRECT_URL = 'admin:index'
+LOGOUT_REDIRECT_URL = '/admin'
